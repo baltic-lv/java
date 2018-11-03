46f100c97ae9e613b606b608f7fd9992e823717d
Juris Guba <liepajnieks@gmail.com>
2018-11-03 11:32:38 +0200

    Init git

---
+++ src/com/balticman/hello/HelloUser_001.java
@@ -0,0 +1,8 @@
+package com.balticman.hello;
+
+public class HelloUser_001 {
+    public static void main(String[] args) {
+        System.out.println("Hello\nJuris Guba");
+    }
+}
+
---
+++ src/com/balticman/hello/SumSimple_002.java
@@ -0,0 +1,15 @@
+package com.balticman.hello;
+
+public class SumSimple_002 {
+    /*2. Write a Java program to print the sum of two numbers.
+    Test Data:
+    74 + 36
+    Expected Output :
+    110*/
+
+    public static void main(String[] args) {
+        System.out.println(74 + 36);
+    }
+}
+
+

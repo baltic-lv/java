d155bdee53638676c016ddc3405f1fa36f0aead2
Juris Guba <liepajnieks@gmail.com>
2018-11-03 14:09:32 +0200

    change Divide

--- src/com/balticman/hello/Divide_003.java
+++ src/com/balticman/hello/Divide_003.java
@@ -1,5 +1,9 @@
 package com.balticman.hello;
-
+/*3. Write a Java program to divide two numbers and print on the screen. Go to the editor
+        Test Data :
+        50/3
+        Expected Output :
+        16*/
 public class Divide_003 {
     public static void main(String[] args) {
         System.out.println("50/3 = "+(50/3));

183d0d24ea93ac3a3e70b1ce5d8259a384b76e9f
Juris Guba <liepajnieks@gmail.com>
2018-11-03 18:45:18 +0200

    New push

---
+++ src/com/balticman/hello/JavaLetters_008.java
@@ -0,0 +1,13 @@
+package com.balticman.hello;
+/* J    a   v     v  a
+   J   a a   v   v  a a
+J  J  aaaaa   V V  aaaaa
+ JJ  a     a   V  a     a*/
+public class JavaLetters_008 {
+    public static void main(String[] args) {
+        System.out.println("   J    a   v     v   a");
+        System.out.println("   J   a a   v   v   a a");
+        System.out.println("J  J  aaaaa   V V   aaaaaa");
+        System.out.println(" JJ  a     a   V   a      a");
+    }
+}
